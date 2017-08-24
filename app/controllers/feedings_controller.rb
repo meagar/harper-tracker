c60f913766edfad72f451995ff3fedd82f073faf
Matthew Eagar <meagar@gmail.com>
2017-08-24 15:51:36 -0400

    Fix start-time bug

--- app/controllers/feedings_controller.rb
+++ app/controllers/feedings_controller.rb
@@ -54,7 +54,7 @@ class FeedingsController < ApplicationController
 
   def create_params
     params.require(:feeding).permit(:amount, :formula, :milk, :notes).tap do |p|
-      p[:start_time] = Time.parse([params[:feeding][:start_hour], params[:feeding][:start_minute]].join(':'))
+      p[:start_time] = Time.parse([params[:feeding][:start_hour], params[:feeding][:start_minute]].join(':') + params[:feeding].delete(:start_ampm))
     end
   end
 
