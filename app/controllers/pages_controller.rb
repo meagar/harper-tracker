ce79cf2b613da97dba2b2d1d7ed3c5a10e479793
Matthew Eagar <meagar@gmail.com>
2017-08-24 22:31:48 -0400

    Update index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@
 class PagesController < ApplicationController
   def index
     @last_feed = Feeding.order(:created_at).last
+    @last_pee = Changing.with_pee.order(:created_at).last
+    @last_poop = Changing.with_poop.order(:created_at).last
   end
 end
