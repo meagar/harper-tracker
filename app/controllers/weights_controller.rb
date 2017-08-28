e85df7a846dee9ec1d0041d8c78d3c0934cb4878
Matthew Eagar <meagar@gmail.com>
2017-08-28 14:36:31 -0400

    Finish CRUD actions for weights

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -1,8 +1,13 @@
 # frozen-string-literal: true
 
 class WeightsController < ApplicationController
+  before_action :find_weigh_in, only: %i(show edit update destroy)
+
   def index
     @weigh_ins = WeighIn.order(weigh_time: :desc).all
+  end
+
+  def show
   end
 
   def new
@@ -19,9 +24,33 @@ class WeightsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @weigh_in.update(update_params)
+      redirect_to @weigh_in, notice: 'Updated'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @weigh_in.destroy!
+    redirect_to :weigh_ins, notice: 'Weigh-in deleted'
+  end
+
   private
+
+  def find_weigh_in
+    @weigh_in = WeighIn.find(params[:id])
+  end
 
   def create_params
     params.require(:weigh_in).permit(:weigh_time, :weight, :notes)
+  end
+
+  def update_params
+    create_params
   end
 end
