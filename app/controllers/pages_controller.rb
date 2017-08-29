0edc35b370e179f3438a4684ac1ece7aaf607812
Matthew Eagar <meagar@gmail.com>
2017-08-29 18:58:26 -0400

    Add last pump to homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,9 @@
 
 class PagesController < ApplicationController
   def index
-    @last_feed = Feeding.order(:start_time).last
-    @last_pee = Changing.with_pee.order(:change_time).last
-    @last_poop = Changing.with_poop.order(:change_time).last
+    @last_feed = Feeding.first
+    @last_pee = Changing.with_pee.first
+    @last_poop = Changing.with_poop.first
+    @last_pump = Pumping.first
   end
 end
