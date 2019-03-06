d68692c2784f4b8fea1068c601393077425bf407
Matthew Eagar <meagar@gmail.com>
2019-03-06 10:11:49 -0500

    Fix feeding form, remove breast stuff

--- app/models/feeding.rb
+++ app/models/feeding.rb
@@ -1,6 +1,9 @@
 # frozen-string-literal: true
 
 class Feeding < ApplicationRecord
+  # Breast feeding is off the table, stub values in
+  before_validation :populate_breasts
+
   validates :start_time, presence: true
   validates :left_breast, presence: true
   validates :right_breast, presence: true
@@ -15,6 +18,10 @@ class Feeding < ApplicationRecord
   scope :last_24, -> { where('start_time >= ?', 24.hours.ago) }
 
   private
+
+  def populate_breasts
+    self.left_breast = self.right_breast = 0
+  end
 
   def must_have_formula_or_milk
     errors.add(:base, 'Must have one or both of Formula or Milk') unless formula || milk
